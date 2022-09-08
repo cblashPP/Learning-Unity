b7627287cfa1e3a45373942b450cbbae3174adf3
Mr. Conrad <62264897+cblashPP@users.noreply.github.com>
2022-09-08 07:32:54 -0700

    Began Plane scripting challenge.
    These two scripts were provided by the class and are broken.  I fixed FollowPlayerX already though.

---
+++ FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = Vector3.right * 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    // LateUpdate is called every frame after Update is finished
+
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
