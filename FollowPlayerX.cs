082da9dde8d9740e69ebef9dc1850b8b05993583
Mr. Conrad <62264897+cblashPP@users.noreply.github.com>
2022-09-08 17:13:11 -0700

    Functionally solved the challenge.
    Cleaning up camera and player speeds, gotta comment too

--- FollowPlayerX.cs
+++ FollowPlayerX.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = Vector3.right * 20f;
+    //private Quaternion offsetRotation = Quaternion.AngleAxis(-90, Vector3.up);
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +17,11 @@ public class FollowPlayerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    // LateUpdate is called every frame after Update is finished
+
+    void LateUpdate()
     {
         transform.position = plane.transform.position + offset;
+        //transform.rotation = offsetRotation;
     }
 }
